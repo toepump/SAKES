afc537de4761088bd2ce38bf8b6a77e2f83c10a2
Mikey Kentaro <mikeykentarocho@gmail.com>
2017-05-24 13:51:10 +0900

    test launched thread

--- tests/encRTtest2.cpp
+++ tests/encRTtest2.cpp
@@ -243,9 +243,12 @@ int main(int argc, char* argv[]){
 
 	//Creation of the thread
 	pthread_t thread1;
+    pthread_t thread2;
 	const char *message1 = "Thread 1";
-	int  iret1;
+    const char *message2 = "Thread 2";
 
+	int  iret1;
+	int  iret2;
 	/*set attribute */
 
 	pthread_attr_t attr1; //Creation of the variable for the attribute
@@ -261,19 +264,25 @@ int main(int argc, char* argv[]){
 	pthread_attr_setschedparam(&attr1, &parm1); //set the scheduling parameter of attr1 as parm1
 
 	iret1 = pthread_create(&thread1, &attr1, testThread1,(void*) message1); //create a thread that launch the print_message_function with the arguments  message1
-	pthread_setschedparam(thread1, SCHED_FIFO, &parm1); // sets the scheduling and parameters of thread1 with SCHED_FIFO and parm1
+    iret2 = pthread_create(&thread2, &attr1, testThread2,(void*) message2);
+    pthread_setschedparam(thread1, SCHED_FIFO, &parm1); // sets the scheduling and parameters of thread1 with SCHED_FIFO and parm1
 														// if it fails, return not 0
+    pthread_setschedparam(thread2, SCHED_FIFO, &parm1);
+
 	//set priority each thread
 	pthread_setschedprio(thread1, 49);
-
+    pthread_setschedprio(thread2, 49);
 	//
 	printf("pthread_create() for thread 1 returns: %d\n",iret1);
+    printf("pthread_create() for thread 2 returns: %d\n",iret2);
 
 	/* Wait till threads are complete before main continues. Unless we  */
 	/* wait we run the risk of executing an exit which will terminate   */
 	/* the process and all threads before the threads have completed.   */
 
 	pthread_join( thread1, NULL);
+    pthread_join( thread2, NULL);
+
 
 	exit(EXIT_SUCCESS);
 }
