c3fa3627a1d4717805016553cec33a0689dd0d81
VBaban <vbabin.44@gmail.com>
2017-06-02 18:16:54 +0900

    correction 11

--- tests/Test_Motor.cpp
+++ tests/Test_Motor.cpp
@@ -66,7 +66,7 @@ void polyEval(double coeffs[], double *x, double *output){
 	}
 	result=result+coeffs1[56];
 	test1=*x;
-	cout << "Print Of Result:  " << result << "Print Of Result:  " << test1<< endl;
+	cout << "Results in function  " << result << "      Value of test:  " << test1<< endl;
 
 	*output=result;
 }
@@ -158,7 +158,8 @@ void *testThread1(void *ptr) {
   	if(ticks_t1%500==0){
   		polyEval(coeffs1, &timePolyEval, &angleTest);
   		test1=coeffs1[0]*pow(timePolyEval,MAXDEGREEPOLY);
-  		cout << "Time:  " << timePolyEval << " Angle:  " << angleTest << " Value of test1:  " << test1 << endl;
+  		cout << "Time:  " << timePolyEval << "     Angle:  " << angleTest << "     Value of test1:  " << test1 << endl;
+  		cout << "    "  << endl;
 
   		timePolyEval=timePolyEval+0.05;
   		if(timePolyEval>0.99){
