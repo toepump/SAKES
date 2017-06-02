86aede5a289ce022a677929ec5e3caee0f9bb89a
VBaban <vbabin.44@gmail.com>
2017-06-02 18:06:22 +0900

    correction8

--- tests/Test_Motor.cpp
+++ tests/Test_Motor.cpp
@@ -55,9 +55,6 @@ int ticks_t2=0; //Incremental value for the thread 2
 const int MAXDEGREEPOLY=57;
 double coeffs1[MAXDEGREEPOLY]={ 2161704178.57744, -7678966834.50137, 7321336263.45535, 0.0, 0.0, 0.0, -4367145721.75599, 0.0, 0.0, 3978221771.45533, 0.0, 0.0, -985186083.378593, 0.0, 0.0, 0.0, 0.0, -2539164036.74563, 0.0, 0.0, 0.0, 5490325943.05484, 0.0, 0.0, 0.0, -7322471014.88356, 0.0, 0.0, 0.0, 8222278890.26401, 0.0, 0.0, 0.0, -10920714548.7223, 0.0, 0.0,16644524938.8166, 0.0, -16364372664.7029, 0.0,8080579968.97197, 0.0, 0.0, -2834740327.51965, 0.0,  1331213566.8815,  1010961461.3028, -2387418495.97765,1659337298.89251, -622192768.04629, 138921217.362358, -18305060.8984875,1320820.91241054, -50395.6593240169, 1594.07054186194, 38.7663699452653,11.8011809149536};
 
-
-
-
 void polyEval(double coeffs[], double *x, double *output){
 
 	int i;
@@ -156,7 +153,7 @@ void *testThread1(void *ptr) {
   	/* do the stuff */
   	if(ticks_t1%500==0){
   		polyEval(coeffs1, &timePolyEval, &angleTest);
-  		test1=coeffs[0]*pow(timePolyEval,57);
+  		test1=coeffs1[0]*pow(timePolyEval,57);
   		cout << "Degree of polynom:  " << MAXDEGREEPOLY << endl;
   		cout << "Time:  " << timePolyEval << " Angle:  " << angleTest << " Value of test1:  " << test1 << endl;
 
