25260ac4a5edcf26fff7e411304196075a2c5f5b
VBaban <vbabin.44@gmail.com>
2017-06-13 15:08:58 +0900

    de

--- tests/TestRT5.cpp
+++ tests/TestRT5.cpp
@@ -258,7 +258,7 @@ int main(int argc, char* argv[]){
 		printf("Problem set policy 3: %d \n", setPolicy);
 	}
 
-	param1.sched_priority = 30;
+	param1.sched_priority = 99;
 	param2.sched_priority = 70;
 	param3.sched_priority = 50;
 
