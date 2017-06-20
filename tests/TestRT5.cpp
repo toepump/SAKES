924e7e178c32e24d2ad32be66adc8d6c63e579cd
VBaban <vbabin.44@gmail.com>
2017-06-20 11:24:15 +0900

    fd

--- tests/TestRT5.cpp
+++ tests/TestRT5.cpp
@@ -386,9 +386,6 @@ void *testThread1(void *ptr) {
 	  		previous_start.tv_sec=start.tv_sec;
 	  		previous_start.tv_nsec=start.tv_nsec;
 
-  			waitTime.tv_sec=0;
-  			waitTime.tv_nsec=1000000-diff.tv_nsec;
-
 			if(ticks_t1==1)
 			{
 	  			waitTime.tv_sec=0;
