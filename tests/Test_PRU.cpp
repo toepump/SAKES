f5fa56bc5f55d998b0350acccae0c6e0e2a92b34
VBaban <vbabin.44@gmail.com>
2017-07-07 10:55:06 +0900

    change open from rdwr to rdonly

--- tests/Test_PRU.cpp
+++ tests/Test_PRU.cpp
@@ -540,7 +540,7 @@ void *testThread1(void *ptr) {
 			cout << "Before"<< endl;
 			/* Open the rpmsg_pru character device file */
 			//fd=open(filename, O_RDWR);
-			pollfds[0].fd = open(filename, O_RDWR);
+			pollfds[0].fd = open(filename, O_RDONLY);
 			cout << "After poll "<< endl;
 			//result = read(fd, readBuf, MAX_BUFFER_SIZE);
 			result = read(pollfds[0].fd, readBuf, MAX_BUFFER_SIZE);
