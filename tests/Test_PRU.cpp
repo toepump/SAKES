c6200fa5f5defec67bb71b07a504b1c2309ffbff
VBaban <vbabin.44@gmail.com>
2017-07-07 11:03:30 +0900

    output in pru is now 65

--- tests/Test_PRU.cpp
+++ tests/Test_PRU.cpp
@@ -545,7 +545,7 @@ void *testThread1(void *ptr) {
 			result = read(pollfds[0].fd, readBuf, MAX_BUFFER_SIZE);
 			cout << "Result "<< result  << endl;
 			if(result > 0){
-			        number= (int*)readBuf;
+			        number= (int)readBuf;
 			        cout << "The results is : " << number << endl;
 			        cout << " " << endl;
 			}else{
