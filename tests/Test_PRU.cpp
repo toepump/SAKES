e0885cbe1e8a7acbb0bb452ea4b0437fe7405587
VBaban <vbabin.44@gmail.com>
2017-07-07 11:46:36 +0900

    test with atoi

--- tests/Test_PRU.cpp
+++ tests/Test_PRU.cpp
@@ -542,10 +542,10 @@ void *testThread1(void *ptr) {
 			//fd=open(filename, O_RDWR);
 			pollfds[0].fd = open(filename, O_RDWR);
 			//result = read(fd, readBuf, MAX_BUFFER_SIZE);
-			result = read(pollfds[0].fd, &readBuf, MAX_BUFFER_SIZE);
+			result = read(pollfds[0].fd, readBuf, MAX_BUFFER_SIZE);
 			cout << "Result "<< result  << endl;
 			if(result > 0){
-			        number= readBuf-'0';
+			        number= atoi(readBuf);
 			        cout << "The readBuf is : " << readBuf << endl;
 			        cout << "The number is : " << number << endl;
 			        cout << " " << endl;
