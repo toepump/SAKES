5e40063ef8155c3a677eafca75399cdb6b274b1b
VBaban <vbabin.44@gmail.com>
2017-07-11 15:10:44 +0900

    sd

--- tests/Test_PRU.cpp
+++ tests/Test_PRU.cpp
@@ -386,7 +386,7 @@ int fileTimespecA(struct timespec *time, int length){
 		timeMilli=time->tv_sec*1000000000+time->tv_nsec;
 		TimeMilliDouble=double(timeMilli)/1000.0;
 		timeMilli=int(TimeMilliDouble);
-		fprintf(fj1,"%d;%f\r\n",i+1,timeMilli);
+		fprintf(fj1,"%d;%d\r\n",i+1,timeMilli);
 		i++ ;
 	}
 	fclose(fj1);
@@ -409,7 +409,7 @@ int fileTimespecB(struct timespec *time, int length){
 		timeMilli=time->tv_sec*1000000000+time->tv_nsec;
 		TimeMilliDouble=double(timeMilli)/1000.0;
 		timeMilli=int(TimeMilliDouble);
-		fprintf(fj1,"%d;%f\r\n",i+1,timeMilli);
+		fprintf(fj1,"%d;%d\r\n",i+1,timeMilli);
 		i++ ;
 	}
 	fclose(fj1);
