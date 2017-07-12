d9300678103c25afa1cf074e2cbd4ade9eb09003
VBaban <vbabin.44@gmail.com>
2017-07-12 10:19:42 +0900

    ony A and B activated

--- CounterPRU/initScript.sh
+++ CounterPRU/initScript.sh
@@ -15,18 +15,18 @@ echo "-Building project"
 	make install
 
 echo "-Configuring pinmux"
-	config-pin -a $HEADER$PIN_NUMBER41 pruin
-	config-pin -q $HEADER$PIN_NUMBER41
-	config-pin -a $HEADER$PIN_NUMBER42 pruin
-	config-pin -q $HEADER$PIN_NUMBER42
+#	config-pin -a $HEADER$PIN_NUMBER41 pruin
+#	config-pin -q $HEADER$PIN_NUMBER41
+#	config-pin -a $HEADER$PIN_NUMBER42 pruin
+#	config-pin -q $HEADER$PIN_NUMBER42
 	config-pin -a $HEADER$PIN_NUMBER43 pruin
 	config-pin -q $HEADER$PIN_NUMBER43
-	config-pin -a $HEADER$PIN_NUMBER44 pruin
-	config-pin -q $HEADER$PIN_NUMBER44
+#	config-pin -a $HEADER$PIN_NUMBER44 pruin
+#   config-pin -q $HEADER$PIN_NUMBER44
 	config-pin -a $HEADER$PIN_NUMBER45 pruin
 	config-pin -q $HEADER$PIN_NUMBER45
-	config-pin -a $HEADER$PIN_NUMBER46 pruin
-	config-pin -q $HEADER$PIN_NUMBER46
+#	config-pin -a $HEADER$PIN_NUMBER46 pruin
+#	config-pin -q $HEADER$PIN_NUMBER46
 
 # echo "********************************************************"
 # echo -e "Done. Now \"echo S > /dev/rpmsg_pru31 && cat /dev/rpmsg_pru31\" and change the logical state of $HEADER$PIN_NUMBER"
